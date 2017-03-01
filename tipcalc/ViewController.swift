ee9363e171318cf94c9de1b4768459957e6aa061
Jonathan Wong <jonwong@outlook.com>
2017-02-28 21:10:14 -0800

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Jonathan Wong on 2/28/17.
+//  Copyright © 2017 Jonathan Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
